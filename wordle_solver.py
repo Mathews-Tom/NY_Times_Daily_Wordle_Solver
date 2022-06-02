a6b26061b88864822653dd24a76d258c842baa07
Tom Mathews <tommathews2007@gmail.com>
2022-06-02 20:03:55 +0530

    Updated Wordle words dict and refactored the code

--- wordle_solver.py
+++ wordle_solver.py
@@ -1,4 +1,3 @@
-import os
 import string
 from collections import OrderedDict
 from datetime import date
@@ -8,10 +7,39 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
 
-URL = "https://www.nytimes.com/games/wordle/index.html"
+NY_TIMES_WORDLE_URL = "https://www.nytimes.com/games/wordle/index.html"
+WORDMASTER_URL = "https://octokatherine.github.io/word-master/"
 WINDOW_WIDTH = 540
 WINDOW_HEIGHT = 960
+
+def load_words(classic_wordle=True):
+    def get_guesses(classic_wordle=True):
+        """Geting guesses either for classic NY Times Wordle or """
+        return __get_wordle_guesses() if classic_wordle else __get_wordmaster_guesses()
+
+    def get_answers(classic_wordle):
+        return __get_wordle_answers() if classic_wordle else __get_wordmaster_answers()
+
+    def __get_wordle_guesses():
+        return __load_words("wordle_guesses.txt")
+
+    def __get_wordle_answers():
+        return __load_words("wordle_answers.txt")
+
+    def __get_wordmaster_guesses():
+        return __load_words("wordmaster_guesses.txt")
+
+    def __get_wordmaster_answers():
+        return __load_words("wordmaster_answers.txt")
+
+    def __load_words(file_name):
+        with open(file_name, "r") as file:
+            words = file.readlines()
+        return [word.strip().upper() for word in words]
+    
+    return (get_guesses(classic_wordle), get_answers(classic_wordle))
 
 def split_word_evaluation(result, guess):
     """Split word evaluation to absent, present and correct"""
@@ -19,11 +47,11 @@ def split_word_evaluation(result, guess):
     present = []
     correct = []
     for i in range(0, len(result)):
-        if result[i] == "a":
+        if result[i] == "A":
             absent.append(guess[i])
-        elif result[i] == "p":
+        elif result[i] == "P":
             present.append([guess[i], i])
-        elif result[i] == "c":
+        elif result[i] == "C":
             correct.append([guess[i], i])
     return (absent, present, correct)
 
@@ -134,33 +162,36 @@ def best_word(possible_words, frequencies):
             best_word = w
     return best_word
 
-def dismiss_initial_tutorial():
+def dismiss_initial_tutorial(actions):
+    """Dismiss initial tutorial popup"""
     actions.move_by_offset(WINDOW_WIDTH//2, WINDOW_HEIGHT//2).click().perform()
     sleep(2)
 
-def enter_guess_get_result(guess):
+def enter_guess_get_result(driver, actions, guess):
+    """Enter guess and get result"""
     actions.send_keys(guess).perform()
     actions.send_keys(Keys.RETURN).perform()
     actions.pause(2)
     row_elements = driver.find_element(By.CSS_SELECTOR, "game-app").shadow_root \
         .find_element(By.CSS_SELECTOR, f"game-row[letters='{guess.lower()}']").shadow_root \
-              .find_elements(By.CSS_SELECTOR, "game-tile") #.find_element(By.CSS_SELECTOR, "game-title[letter='a']")
+              .find_elements(By.CSS_SELECTOR, "game-tile")
     evaluation = OrderedDict()
     for idx, (ch, element) in enumerate(zip(guess, row_elements)):
         evaluation[f"{ch}_{idx+1}"] = element.get_attribute('evaluation')
     return evaluation
 
 def convert_evaluation_dict_to_result(evaluation):
-    return "".join([e[0] for e in evaluation.values()])
+    """Convert evaluation dict to result string with 'a', 'p' and 'c' representing absent, present and correct"""
+    return "".join([e.upper()[0] for e in evaluation.values()])
     
 
-def wordleSolver(possible_words):
+def wordle_solver(browser_driver, browser_actions, possible_words):
     """Prompts you to solve Wordle"""
     print("Welcome to the Wordle Solver!")
     print(f"Let's solve today's ({date.today()}) New York Times Wordle\n")
     
-    driver.get(URL)
-    dismiss_initial_tutorial()
+    browser_driver.get(NY_TIMES_WORDLE_URL)
+    dismiss_initial_tutorial(browser_actions)
 
     game_play = OrderedDict()
     suggestion = best_word(possible_words, get_letter_frequency(possible_words))
@@ -169,11 +200,11 @@ def wordleSolver(possible_words):
     # del possible_words[0: diff]
     
     guess = suggestion
-    game_play[guess] = enter_guess_get_result(guess)
+    game_play[guess] = enter_guess_get_result(browser_driver, browser_actions, guess)
     print(f"Attempt 1:: {guess} -> {game_play[guess]}\n")
     result = convert_evaluation_dict_to_result(game_play[guess])
     counter = 1
-    while result != "ccccc" and counter < 6:
+    while result != "CCCCC" and counter < 6:
         possible_words = remove_incorrect_words(result, guess, possible_words)
         print(f"Possible Words:: {possible_words}")
         if len(possible_words) == 0:
@@ -181,35 +212,31 @@ def wordleSolver(possible_words):
         suggestion = best_word(possible_words, get_letter_frequency(possible_words))
         print("The next suggested word is:", suggestion)
         guess = suggestion
-        game_play[guess] = enter_guess_get_result(guess)
+        game_play[guess] = enter_guess_get_result(browser_driver, browser_actions, guess)
         print(f"Attempt {counter + 1}:: {guess} -> {game_play[guess]}\n")
         result = convert_evaluation_dict_to_result(game_play[guess])
         counter += 1
-    if len(possible_words) == 0:
-        print("Oh no! You made a mistake entering one of your results. Please try again.")
-    elif counter == 6 and result != "ggggg":
+    if counter == 6 and result != "CCCCC":
         print("Number of guesses exceeded, sorry we failed!")
     else:
         print(f"Congratulations! We solved today's Wordle, \"{guess}\", in {counter} guesses.")
 
+def play_wordle(classic_wordle=True):
+    browser = webdriver.Chrome(ChromeDriverManager().install())
+    browser.set_window_size(WINDOW_WIDTH, WINDOW_HEIGHT)
+    actions = ActionChains(browser)
+    browser.implicitly_wait(5)
 
-def load_words(file_name):
-    with open(file_name) as file:
-        words = file.readlines()
-    return [word.strip().upper() for word in words]
+    if classic_wordle:
+        wordle_words, all_words = load_words(classic_wordle)
 
-possible_words = load_words("wordle_words.txt")
+        try:
+            wordle_solver(browser, actions, wordle_words)
+        finally:
+            sleep(5)
+            browser.quit()
 
-options = Options()
-options.add_argument("--disable-notifications")
-chrome_driver_path = os.getcwd() + "/chromedriver"
-driver = webdriver.Chrome(options=options, executable_path=chrome_driver_path)
-driver.set_window_size(WINDOW_WIDTH, WINDOW_HEIGHT)
-actions = ActionChains(driver)
-driver.implicitly_wait(5)
 
-try:
-    wordleSolver(possible_words)
-finally:
-    sleep(5)
-    driver.quit()
+if __name__ == "__main__":
+    classic_wordle = True
+    play_wordle(classic_wordle)
